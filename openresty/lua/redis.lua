9f1d3dbb7971fe70b6c52adf8efa69521d695c24
zhongheng <260789672@qq.com>
2020-07-09 19:00:48 +0800

    【修改】加入JWT 验证LUA依赖包，redis.lua增加TOKEN相关校验流程

--- openresty/lua/redis.lua
+++ openresty/lua/redis.lua
@@ -1,11 +1,13 @@
 local redis = require "resty.redis"
-local cjson = require("cjson")
+local cjson = require"cjson"
+local jwt = require "resty.jwt"
 
 local red = redis:new()
 
 red:set_timeouts(1000, 1000, 1000) -- 1 sec
 red:connect("127.0.0.1", 6379)
 
+-- 检查HEADER是否合法
 local headers = ngx.req.get_headers()
 local authorization = headers["Authorization"]
 if authorization == nil then
@@ -14,15 +16,25 @@ if authorization == nil then
 end
 ngx.log(ngx.INFO, " authorization:", authorization)
 local accessToken = string.gsub(authorization, "Bearer ", '', 1) --替换完似乎前面有个不知道什么符号不对
---local accessToken = string.sub(authorization,8,string.len(authorization))
 ngx.log(ngx.INFO, " accessToken:", accessToken)
 
+-- 校验TOKEN是否合法
+local secret = "SALT"
+local jwt_obj = jwt:verify(secret, accessToken)
+ngx.log(ngx.INFO, " jwt_obj.verified:", jwt_obj.verified)
+
+if jwt_obj.verified == false then
+    ngx.say('{"code":"400100", "data":"www.baidu.com", "msg":"不合法的TOKEN", "success":"false"}')
+    return
+end
+
+-- REDIS获取TOKEN是否存在
 local accessTokenKey = "sso:oidc:api:accessToken:access:"..accessToken
 ngx.log(ngx.INFO, " accessTokenKey:", accessTokenKey)
 local res,err = red:get(accessTokenKey)
 ngx.log(ngx.INFO, " res:", res)
 if res == ngx.null then
-    ngx.say('{"code":"400100", "data":"www.baidu.com", "msg":"登录已失效或无效的TOKEN", "success":"false"}')
+    ngx.say('{"code":"400100", "data":"www.baidu.com", "msg":"登录已失效", "success":"false"}')
     return
 else
     -- TODO 分发到后端对应服务
